ce21e9d2dc61ca70fa63b21d5b40203cca5b59c7
White Nguyen <whitenguyen0909@gmail.com>
2022-05-16 16:21:49 -0600

    added more bootstrap for the deck list

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,7 +5,6 @@ import { listDecks } from "../utils/api";
 
 import { DeckItemLink } from "../Decks/DeckItemLink.js";
 
-
 export default function Home() {
   const [deckList, setDeckList] = useState([]);
   const userouteMatch = useRouteMatch();
@@ -40,14 +39,25 @@ export default function Home() {
   //   console.log(deckList);
   return (
     <div>
-      <div>
-        <Link to="/decks/new"> Create Deck </Link>
+      <div className="mb-2">
+        <Link to="/decks/new" className="btn btn-secondary text-white">
+          {" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="white"
+            class="bi bi-plus"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+          </svg>{" "}
+          Create Deck{" "}
+        </Link>
       </div>
-      <div>{deckItemLinks}</div>
+      <div className="list-group">{deckItemLinks}</div>
       <div>
-        <Switch>
-
-        </Switch>
+        <Switch></Switch>
         <ul></ul>
       </div>
     </div>

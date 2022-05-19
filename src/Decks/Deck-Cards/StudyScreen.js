b03ea85cb3ac890f8725c33d36615fbbe44911f2
White Nguyen <whitenguyen0909@gmail.com>
2022-05-19 14:05:59 -0600

    added extra icons

--- src/Decks/Deck-Cards/StudyScreen.js
+++ src/Decks/Deck-Cards/StudyScreen.js
@@ -99,7 +99,23 @@ export default function StudyScreen() {
             to={`/decks/${deckInfo.id}/cards/new`}
             className="btn btn-primary"
           >
-            Add Cards
+            <div>
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                className="bi bi-plus-lg"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+                />
+              </svg>
+            </div>
+            <div>Add Cards</div>
           </Link>
         </div>
       )}
--- src/Decks/DeckDisplay.js
+++ src/Decks/DeckDisplay.js
@@ -27,7 +27,7 @@ export const DeckDisplay = ({ deckInfo, handleDelete, cardList }) => {
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-pencil-fill"
+                className="bi bi-pencil-fill"
                 viewBox="0 0 16 16"
               >
                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
@@ -45,11 +45,11 @@ export const DeckDisplay = ({ deckInfo, handleDelete, cardList }) => {
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-journal-bookmark-fill"
+                className="bi bi-journal-bookmark-fill"
                 viewBox="0 0 16 16"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M6 1h6v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8V1z"
                 />
                 <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z" />
@@ -65,14 +65,14 @@ export const DeckDisplay = ({ deckInfo, handleDelete, cardList }) => {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
+                width="24"
+                height="24"
                 fill="currentColor"
-                class="bi bi-plus-lg"
+                className="bi bi-plus-lg"
                 viewBox="0 0 16 16"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
                 />
               </svg>

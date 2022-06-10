0e7ea356c8609b16e12f76cd82e558ab7f894e31
White Nguyen <whitenguyen0909@gmail.com>
2022-06-10 10:07:25 -0600

    removed vercel, will use heroku instead

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -30,7 +30,7 @@ export default function Home() {
     const controller = new AbortController();
     const result = window.confirm("Delete this deck?");
     if (result) {
-      await deleteDeck(deckInfo.id,controller.signal);
+      await deleteDeck(deckInfo.id, controller.signal);
       history.go(0);
     }
   };
@@ -48,7 +48,6 @@ export default function Home() {
     <div>
       <div className="mb-2">
         <Link to="/decks/new" className="btn btn-secondary text-white">
-          {" "}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="32"
@@ -58,8 +57,8 @@ export default function Home() {
             viewBox="0 0 16 16"
           >
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-          </svg>{" "}
-          Create Deck{" "}
+          </svg>
+          Create Deck
         </Link>
       </div>
       <div className="list-group">{homeDecks}</div>
